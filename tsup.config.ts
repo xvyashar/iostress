a473a8d5026fd3f0f7e6a2eeb1f353c0e720e633
XV Yashar <xvyashar@gmail.com>
2025-04-15 12:35:41 +0330

    fix: separate test-runner worker from dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  format: ['cjs', 'esm'],
-  entry: ['./src/index.ts'],
-  dts: true,
-  shims: true,
-  skipNodeModulesBundle: true,
-  clean: true,
-});
+export default defineConfig([
+  {
+    format: ['cjs', 'esm'],
+    entry: ['./src/index.ts'],
+    dts: true,
+    shims: true,
+    skipNodeModulesBundle: true,
+    clean: true,
+  },
+  {
+    format: ['cjs', 'esm'],
+    entry: ['./src/runner/test-runner.ts'],
+    dts: true,
+    shims: true,
+    skipNodeModulesBundle: true,
+    clean: true,
+  },
+]);
